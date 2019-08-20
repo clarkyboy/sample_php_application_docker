d2887849ad15406946c6aaf78722ba06adc6bce6
Christian C. Barral <christian@appleid003s-MacBook-Pro.local>
2019-08-20 15:47:16 +0800

    modified backend

--- src/db/item.php
+++ src/db/item.php
@@ -2,7 +2,14 @@
 
 require 'connection.php';
 
-function add_item($item_serial_no, $item_name, $item_bought, $item_vendor, $item_){
+function add_item($item_serial_no, $item_name, $item_bought, $item_quantity, $item_vendor, $item_type){
+
+    $sql = "INSERT INTO items (item_serial_id, item_name, item_bought, item_quantity, item_vwendor, item_type, item_type)
+            VALUES ('$item_serial_no', '$item_name', '$item_bought', '$item_quantity', '$item_vendor', '$item_type')";
+
+    $conn = connection();
+    $result = $conn->query($sql);
+    return $sql;
 
 
 }
--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<div class="flex justify-center">
+<div class="flex justify-center mt-5">
     <form class="w-full max-w-lg">
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -34,19 +34,19 @@
         <div class="flex flex-wrap -mx-3 mb-2">
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                    City
+                    Item Date Bought
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="date" placeholder="mm-dd-yy" name="item_vendor">
             </div>
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                    State
+                    Item Type
                 </label>
                 <div class="relative">
-                    <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                        <option>New Mexico</option>
-                        <option>Missouri</option>
-                        <option>Texas</option>
+                    <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" name="item_type">
+                        <option value="C">Computer Peripherals</option>
+                        <option value="O">Office Supplies</option>
+                        <option value="D">Texas</option>
                     </select>
                     <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -55,13 +55,13 @@
             </div>
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                    Zip
+                    Item Quantity
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="number" placeholder="90210" name="item_quantity">
             </div>
         </div>
 
-        <input type="submit" value="Add Item" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        <input type="submit" value="Add Item" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" name="add">
     </form>
 </div>
 </body>
